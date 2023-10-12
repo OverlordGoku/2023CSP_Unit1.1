62bd35434c5bddea8939fd5e83cb3ced156a3d3b
12862 <daniel.gosses@vtsd.com>
2023-10-12 07:55:19 -0400

    Hello

--- 1-1-6/Step 5.py
+++ 1-1-6/Step 5.py
@@ -7,14 +7,34 @@ Spider.pensize(40)
 Spider.circle(20)
 Legs = 8
 LengthOfLegs = 70
-Spacing = 360 / Legs
+Spacing = 360 / Legs-20
 Spider.pensize(5)
 Number = 0
 while (Number < Legs):
   Spider.goto(0, 20)
-  Spider.setheading(Spacing * Number)
-  Spider.forward(LengthOfLegs)
+
+
+
+  if(Number < 4):
+    Spider.setheading(Spacing * Number-45)
+    Spider.forward(LengthOfLegs)
+
+  else:
+    Spider.setheading(Spacing * Number+45)
+    Spider.forward(LengthOfLegs)
   Number = Number + 1
+
+for loop in range(2):
+  Spider.penup()
+  Spider.goto( 6, 27)
+Spider.pendown()
+Spider.color("Red")
+Spider.pensize(10)
+Spider.circle(5)
+Spider.penup()
+Spider.goto( -20, 27)
+Spider.pendown()
+Spider.circle(5)
 Spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
