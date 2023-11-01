20734fbfca46754e2bf32f655d6f50f7f38da353
12862 <daniel.gosses@vtsd.com>
2023-11-01 10:07:35 -0400

    Hello

---
+++ 1-1-9/Project 1 SpiroGraph.py
@@ -0,0 +1,28 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.speed(100)
+
+painter.penup()
+x = -100
+y = 0
+painter.goto(x, y)
+
+
+painter.pendown()
+
+for numStars in range(5):
+    for triangle in range(15):
+        painter.pendown()
+        painter.forward(50)
+        painter.left(120)
+        painter.left(45)
+        painter.penup()
+    x += 50
+    y += 50
+    painter.goto(x, y)
+
+
+wn = trtl.Screen()
+wn.mainloop()
